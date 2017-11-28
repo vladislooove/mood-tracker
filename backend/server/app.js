7606f9c210a43f6cd8f0b53ea8abd2e1951222b6
fadetoblack1 <fadetoblack1@yandex.ru>
2017-11-29 00:55:40 +0200

    started auth strategy, npm script to start app

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
 const serverPort = require('./etc/config.json').serverPort;
 const db =require('./utils/DataBaseUtils.js');
 
@@ -8,9 +10,29 @@ db.setUpConnection();
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+// configure app
 
-const server = app.listen(serverPort, () => {
+app.use(cors());
+app.use(bodyParser.json());    
+
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+    console.log('serializeUser: ' + user._id);
+    done(null, user._id);
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user){
+        console.log(user);
+        if(!err) done(null, user);
+        else done(err, null);
+    });
+}); 
+
+// api
+app.get('/', (req, res)=>{
+    res.send('home')
+});
+
+app.listen(serverPort, () => {
     console.log('Server is running on ' + serverPort);
 })
