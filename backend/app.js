292185bc49d0f3ccbebe569bae05bff67a489505
fadetoblack1 <fadetoblack1@yandex.ru>
2017-11-30 00:00:36 +0200

    removed extra auth strategies, use only local

--- backend/app.js
+++ backend/app.js
@@ -44,4 +44,4 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 
 // launch ======================================================================
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('server is running at: ' + port);
