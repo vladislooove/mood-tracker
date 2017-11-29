72a32f9873e07a3d361bd256d70f4d941808f258
fadetoblack1 <fadetoblack1@yandex.ru>
2017-11-29 23:50:59 +0200

    stolen login && signup strategy

--- backend/server/utils/DataBaseUtils.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const config = require('../etc/config.json');
-
-const setUpConnection = function(){
-    mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`);
-}
-
-module.exports.setUpConnection = setUpConnection;
